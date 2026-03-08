06621cf622c13f9d812544abf6b9aa7ae55c769f
Aifi <aifi_official_ff@internet.ru>
2026-03-08 22:57:26 +0500

    i am *d*ot
    ...

--- src/NLUtils/Installer.py
+++ src/NLUtils/Installer.py
@@ -2,7 +2,7 @@
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-from NLUtils.Logger import NLLogger,ConColors
+from .Logger import NLLogger,ConColors
 from pathlib import Path
 import shutil, subprocess
 class NLInstaller:
